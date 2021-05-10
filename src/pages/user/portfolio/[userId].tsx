8935e08a68aceb7a94906eafdc2e3a260ffee303
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-05-10 10:21:14 -0400

    Update [userId].tsx

--- src/pages/user/portfolio/[userId].tsx
+++ src/pages/user/portfolio/[userId].tsx
@@ -1,6 +1,6 @@
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Portfolio } from "src/components/portfolio/Portfolio";
 import { urqlClient } from "src/urql/urqlClient";
 import { DoubleColLayout } from "../../_doubleColLayout";
@@ -9,10 +9,13 @@ interface portfolioProps {}
 
 const portfolio: React.FC<portfolioProps> = () => {
   const router = useRouter();
-  if (!router.query.userId) {
-    router.push("/onboarding/hiveLogin");
-  }
-  const userId = router.query.userId as string;
+  const [userId, setUserId] = useState("");
+  useEffect(() => {
+    if (!router.query.userId) {
+      router.push("/onboarding/hiveLogin");
+    }
+    setUserId(router.query.userId as string | "");
+  }, []);
   return (
     <DoubleColLayout>
       <Portfolio mb={4} pl={6} userId={userId} />
